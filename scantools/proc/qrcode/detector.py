ae8b8dbc4f4c6efc1b042e3d82a1efc4b5c0dac5
Pablo Speciale <Pablo.Speciale@microsoft.com>
2024-01-15 16:29:54 +0100

    CR

--- scantools/proc/qrcode/detector.py
+++ scantools/proc/qrcode/detector.py
@@ -115,10 +115,7 @@ class QRCodeDetector:
                 qr_code = {
                     "id": row[0],
                     "points2D": [
-                        [float(row[1]), float(row[2])],
-                        [float(row[3]), float(row[4])],
-                        [float(row[5]), float(row[6])],
-                        [float(row[7]), float(row[8])],
+                        list(map(float, row[i : i + 2])) for i in [1, 3, 5, 7]
                     ],
                 }
                 self.qrcodes.append(qr_code)
