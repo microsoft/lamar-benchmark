fe91eb08d1a495d0991a6aa48706feb976ce0203
Pablo Speciale <Pablo.Speciale@microsoft.com>
2024-01-29 01:04:19 +0100

    remove file

--- docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-PS4='\033[1;96m$(date +%H:%M:%S)\033[0m '
-set -exo pipefail
-
-docker build --target builder -t lamar:builder -f Dockerfile .
-docker build --target scantools -t lamar:scantools -f Dockerfile .
-docker build --target pyceres -t lamar:pyceres -f Dockerfile .
