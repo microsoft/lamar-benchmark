de200e119036172e34c40361c1ba40fdea9c9b18
Mihai Dusmanu <mihai.dusmanu@gmail.com>
2025-05-11 12:19:45 +0200

    Fix.

--- lamar/combine_results.py
+++ lamar/combine_results.py
@@ -51,33 +51,28 @@ if __name__ == '__main__':
         help="Output directory where the zip will be saved.",
         required=True,
     )
-    args = parser.parse_args()
-    
-    # Define all arguments and their human-readable names.
-    arg_paths = {
-        ("CAB", "phone"): args.cab_phone_path,
-        ("LIN", "phone"): args.lin_phone_path,
-        ("HGE", "phone"): args.hge_phone_path,
-        ("CAB", "hololens"): args.cab_hololens_path,
-        ("LIN", "hololens"): args.lin_hololens_path,
-        ("HGE", "hololens"): args.hge_hololens_path,
-    }
-    
+    args = parser.parse_args().__dict__
+    output_dir = args.pop("output_dir")
+
     # Generate timestamp for the zip file name.
     timestamp = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
-    zip_filename = args.output_dir / f"result_{timestamp}.zip"
+    zip_filename = output_dir / f"result_{timestamp}.zip"
     
     # Create the zip file with existing paths.
     logger.info(f"Creating zip file at {zip_filename}")
-    args.output_dir.mkdir(parents=True, exist_ok=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
     with zipfile.ZipFile(zip_filename, "w", zipfile.ZIP_DEFLATED) as zipf:
-        for split, path in arg_paths.items():
+        for name, path in args.items():
+            split = name.split("_")
+            assert len(split) == 3
+            assert split[-1] == "path"
+            split = split[:-1]
             if path is None:
-                logger.warning(f"No path provided for [{split}], skipping")
+                logger.warning(f"No path provided for [{split}], skipping...")
                 continue
             assert path.exists()
             assert path.is_file()
             assert path.suffix == ".txt", f"Expected .txt file, got {path.suffix}"
             logger.info(f"Adding [{split}] file from {path} to zip")
-            zipf.write(path, arcname=f"{split[0]}_query_{split[1]}.txt")
+            zipf.write(path, arcname=f"{split[0].upper()}_query_{split[1]}.txt")
     logger.info(f"Successfully created zip file at {zip_filename}")
